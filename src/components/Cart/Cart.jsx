5e11b946816304f30842dc5b95a2cbba6fd431fa
Your Name <hridhyapaul77@gmail.com>
2023-03-30 16:39:46 +0600

    Save cart to local storege

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({cart}) => {
-    console.log(cart)
     // const cart = props.cart // option 1
     // const {cart} = props // option 2
 
