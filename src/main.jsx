408d4791235cc853ab3c5c3a3dcf91a2af32b146
Hridhyapaul <hpdesign2021@gmail.com>
2023-04-06 21:37:08 +0600

    Order summary added to 'Order review' page

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Layout/Home';
+import Shop from './components/Shop/Shop';
+import Order from './components/Order/Order';
+import Inventory from './components/Inventory/Inventory';
+import Login from './components/Login/Login';
+import cartProductsLoader from './Loaders/CartProductsLoader';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/',
+        element: <Shop></Shop>
+      },
+      {
+        path: '/order review',
+        element: <Order></Order>,
+        loader: cartProductsLoader
+      },
+      {
+        path: '/manage inventory',
+        element: <Inventory></Inventory>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
